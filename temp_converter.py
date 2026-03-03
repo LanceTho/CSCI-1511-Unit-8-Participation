858ac482184f32b674def357e90310c44633b13f
Lance Thongsavanh <147730520+LanceTho@users.noreply.github.com>
2026-03-03 17:03:33 -0500

    Uploaded temperature converter file from Blackboard

---
+++ temp_converter.py
@@ -0,0 +1,7 @@
+
+# rewrite to use functions 
+
+
+temp_celsius = float(input("Enter temperature in Celsius: "))
+temp_fahrenheit = (temp_celsius * 9/5) + 32
+print(f"{temp_celsius} degrees Celsius is equal to {temp_fahrenheit} degrees Fahrenheit.")
