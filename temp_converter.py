fbdad41f2e7fa4e8c08faa19fe259848fac01297
Lance Thongsavanh <lancer8106@gmail.com>
2026-03-03 17:43:08 -0500

    Rewrote the program into a task-based function, side-effect function, and an orchestrator.

--- temp_converter.py
+++ temp_converter.py
@@ -2,6 +2,18 @@
 # rewrite to use functions 
 
 
-temp_celsius = float(input("Enter temperature in Celsius: "))
-temp_fahrenheit = (temp_celsius * 9/5) + 32
-print(f"{temp_celsius} degrees Celsius is equal to {temp_fahrenheit} degrees Fahrenheit.")
+
+def convert_to_fahrenheit(temp_celsius: float) -> float:
+    return (temp_celsius * 9/5) + 32
+
+def display_results(temp_celsius: float, temp_fahrenheit: float) -> None:
+    print(f"{temp_celsius} degrees Celsius is equal to {temp_fahrenheit} degrees Fahrenheit.")
+
+def process_conversion(temp_celsius: float) -> None:
+    temp_fahrenheit: float = convert_to_fahrenheit(temp_celsius)
+
+    display_results(temp_celsius, temp_fahrenheit)
+
+if __name__ == "__main__":
+    temp_celsius: float = float(input("Enter temperature in Celsius: "))
+    process_conversion(temp_celsius)
